55bcb72d18cd6e3cc61195891260d17224ba2ce2
Empoliam <liamjwright@btinternet.com>
2022-07-13 22:43:49 +0100

    Initial Commit

---
+++ src/main/java/dev/patchi/surveymod/SurveyMod.java
@@ -0,0 +1,36 @@
+package dev.patchi.surveymod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(SurveyMod.MOD_ID)
+public class SurveyMod {
+
+    public static final String MOD_ID = "surveymod";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public SurveyMod() {
+
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        
+        eventBus.addListener(this::setup);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+}
